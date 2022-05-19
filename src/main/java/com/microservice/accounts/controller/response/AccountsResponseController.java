4f6d040cefe85629983ce5e1b0ae3428fc80e0f9
robert-er <robert.roza.pl@gmail.com>
2022-05-19 03:28:39 +0200

    -added GET endpoint allower, -moved application properties to configuration profile

--- src/main/java/com/microservice/accounts/controller/response/AccountsResponseController.java
+++ src/main/java/com/microservice/accounts/controller/response/AccountsResponseController.java
@@ -4,9 +4,12 @@ import com.microservice.accounts.dto.AccountDto;
 import com.microservice.accounts.mapper.AccountMapper;
 import com.microservice.accounts.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -14,11 +17,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AccountsResponseController {
 
+    @Value("${application.allow-get-accounts}")
+    private boolean allowGetAccounts;
+
     private final AccountMapper accountMapper;
     private final AccountService accountService;
 
     @GetMapping("/v1/accounts")
     public List<AccountDto> getAccounts(@RequestParam Long customerId) {
+        if (!allowGetAccounts) {
+            throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "GET accounts is disabled.");
+        }
         return accountMapper.mapToAccountDtoList(accountService.findByCustomerId(customerId));
     }
 }
